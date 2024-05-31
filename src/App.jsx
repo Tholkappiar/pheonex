c6041f85d26e58e4ab1e9a43f8dc6bd6888e2370
Tholkappiar <tholkappiar2003@gmail.com>
2024-05-31 20:52:32 +0530

    initial commit with navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+	return (
+		<div className="bg-hero-bg min-h-screen">
+			<Navbar />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+	return (
+		<div className="bg-transparent flex justify-between container mx-auto p-8">
+			<div className="text-xl font-semibold">Pheonex</div>
+			<div className="flex gap-10">
+				<div className="font-semibold">Home</div>
+				<div className="font-semibold">Home</div>
+				<div className="font-semibold">Home</div>
+				<div className="font-semibold">Home</div>
+				<div className="font-semibold">Home</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
