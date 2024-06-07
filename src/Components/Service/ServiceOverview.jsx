21773604eae036319ea48389dae9b1569b1d6d30
Tholkappiar <tholkappiar2003@gmail.com>
2024-06-07 21:50:30 +0530

    refactor: service page

--- src/Components/Service/ServiceOverview.jsx
+++ src/Components/Service/ServiceOverview.jsx
@@ -1,15 +1,17 @@
 import search from "../../assets/search.svg";
 import grid from "../../assets/grid.svg";
 import persons from "../../assets/persons-connect.svg";
+import arrowDown from "../../assets/images/arrow-bottom.png";
+import arrowUp from "../../assets/images/arrow-top.png";
 
 const ServiceOverview = () => {
 	return (
-		<div className="h-[400px] sm:h-[500px] 2xl:h-[600px] text-gray-600">
-			<div className="flex flex-col items-center my-20">
+		<div className="h-screen text-gray-600">
+			<div className="flex flex-col items-center">
 				<p className="text-3xl sm:text-5xl font-Noto-Sans-JP font-bold">
 					Heading
 				</p>
-				<p className="text-base sm:text-lg italic font-Noto-Sans-JP sm:w-2/3 mt-4 sm:mt-6 text-center">
+				<p className="text-base sm:text-lg italic font-Noto-Sans-JP sm:w-2/3 mt-4 sm:mt-8 text-center">
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,
 					dolorum mollitia non totam doloribus, fugit nobis esse sunt nam
 					corporis hic, omnis quae optio illo voluptate unde inventore ipsam!
@@ -17,25 +19,54 @@ const ServiceOverview = () => {
 					dolore consectetur?
 				</p>
 			</div>
-			<div className="flex justify-center gap-20 text-center tracking-wider">
-				<div className="size-64 rounded-md shadow-md flex flex-col justify-center">
+			<div className="flex justify-center gap-20 text-center tracking-wider mt-32">
+				{/* <div className="size-64 rounded-md shadow-md flex flex-col justify-center">
 					<img src={search} alt="board" className="size-14 mx-auto" />
 					<p className="mt-4">
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto,
 						laudantium!
 					</p>
-				</div>
-				<div className="size-64 rounded-md shadow-md flex flex-col justify-center">
-					<img src={grid} alt="board" className="size-14 mx-auto" />
-					<p className="mt-4">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
-						voluptas.
+				</div> */}
+				<div className="w-[170px] h-[170px]">
+					<div className="relative w-full h-full">
+						<div className="w-full h-full bg-indigo-600 rotate-45 rounded-lg"></div>
+						<div className="absolute inset-0 bg-white shadow-xl flex justify-center items-center">
+							<img src={search} alt="board" className="w-[56px] h-[56px]" />
+						</div>
+					</div>
+					<p className="mt-20 text-center">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto,
+						laudantium!
 					</p>
 				</div>
-				<div className="size-64 rounded-md shadow-md flex flex-col justify-center">
-					<img src={persons} alt="board" className="size-14 mx-auto" />
-					<p className="mt-4">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam!
+				<div className="flex items-center">
+					<img src={arrowUp} alt="" className="w-[120px] h-[60px]" />
+				</div>
+				<div className="w-[170px] h-[170px]">
+					<div className="relative w-full h-full">
+						<div className="w-full h-full bg-indigo-600 rotate-45 rounded-lg"></div>
+						<div className="absolute inset-0 bg-white shadow-xl flex justify-center items-center">
+							<img src={grid} alt="board" className="w-[56px] h-[56px]" />
+						</div>
+					</div>
+					<p className="mt-20 text-center">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto,
+						laudantium!
+					</p>
+				</div>
+				<div className="flex items-center">
+					<img src={arrowDown} alt="" className="w-[120px] h-[60px]" />
+				</div>
+				<div className="w-[170px] h-[170px]">
+					<div className="relative w-full h-full">
+						<div className="w-full h-full bg-indigo-600 rotate-45 rounded-lg"></div>
+						<div className="absolute inset-0 bg-white shadow-xl flex justify-center items-center">
+							<img src={persons} alt="board" className="w-[56px] h-[56px]" />
+						</div>
+					</div>
+					<p className="mt-20 text-center">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto,
+						laudantium!
 					</p>
 				</div>
 			</div>
--- src/Components/Service/Services.jsx
+++ src/Components/Service/Services.jsx
@@ -4,44 +4,51 @@ import appDev from "../../assets/appDev.svg";
 import digitalMarketing from "../../assets/digitalMarketing.svg";
 
 const Services = () => {
+	const services = [
+		{
+			title: "Web development",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia, dicta suscipit culpa nesciunt tenetur?",
+			image: websiteDev,
+		},
+		{
+			title: "App development",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia, dicta suscipit culpa nesciunt tenetur?",
+			image: appDev,
+		},
+		{
+			title: "Graphic Design",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia, dicta suscipit culpa nesciunt tenetur?",
+			image: graphicDesign,
+		},
+		{
+			title: "Digital Marketing",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia, dicta suscipit culpa nesciunt tenetur?",
+			image: digitalMarketing,
+		},
+	];
+
 	return (
 		<div className="h-[400px] sm:h-[500px] 2xl:h-[600px] text-gray-600 mt-20">
 			<p className="text-3xl sm:text-5xl font-Noto-Sans-JP font-bold text-center">
 				Services we DO
 			</p>
-			<div className="flex gap-20 justify-center my-20 text-center tracking-wider">
-				<div className="size-72  rounded-lg shadow-md flex flex-col items-center justify-center">
-					<img src={websiteDev} alt="" className="size-10" />
-					<p className="my-2 font-semibold uppercase">Web development</p>
-					<p className="my-4 text-sm p-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-						officia, dicta suscipit culpa nesciunt tenetur?
-					</p>
-				</div>
-				<div className="size-72  rounded-lg shadow-md flex flex-col items-center justify-center">
-					<img src={appDev} alt="" className="size-10" />
-					<p className="my-2 font-semibold uppercase">App development</p>
-					<p className="my-4 text-sm p-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-						officia, dicta suscipit culpa nesciunt tenetur?
-					</p>
-				</div>
-				<div className="size-72  rounded-lg shadow-md flex flex-col items-center justify-center">
-					<img src={graphicDesign} alt="" className="size-10" />
-					<p className="my-2 font-semibold uppercase">Graphic Design</p>
-					<p className="my-4 text-sm p-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-						officia, dicta suscipit culpa nesciunt tenetur?
-					</p>
-				</div>
-				<div className="size-72  rounded-lg shadow-md flex flex-col items-center justify-center">
-					<img src={digitalMarketing} alt="" className="size-10" />
-					<p className="my-2 font-semibold uppercase">Digital Marketing</p>
-					<p className="my-4 text-sm p-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-						officia, dicta suscipit culpa nesciunt tenetur?
-					</p>
-				</div>
+			<div className="flex flex-col gap-20 justify-center items-center flex-wrap sm:flex-row my-20 text-center tracking-wider p-4">
+				{services.map((service) => (
+					<div
+						key={service.title}
+						className="size-72  rounded-lg shadow-md flex flex-col items-center justify-center group hover:shadow-xl duration-300"
+					>
+						<img src={service.image} alt="" className="size-10" />
+						<p className="my-2 font-semibold uppercase group-hover:text-indigo-600">
+							{service.title}
+						</p>
+						<p className="my-4 text-sm p-2">{service.description}</p>
+					</div>
+				))}
 			</div>
 		</div>
 	);
