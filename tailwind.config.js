d8a0864d143a800aa720c5437d903a3931cd661b
Tholkappiar M <tholkappiarm@Tholkappiars-MacBook-Air.local>
2024-07-09 20:40:30 +0530

    refactor: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,jsx}"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"hero-bg": "url('hero-bg.jpg')",
-				"big-01": "url('big01.jpg')",
-				"big-02": "url('big02.jpg')",
-				serviceBanner1: "url('bg-banner.png')",
-				ServiceHero: "url('serviceHero.jpg')",
-			},
-			fontFamily: {
-				"Noto-Sans-JP": ["Noto Sans JP", "sans-serif"],
-			},
-			keyframes: {
-				"scale-up": {
-					"0%": { transform: "scale(1)" },
-					"100%": { transform: "scale(1.25)" },
-				},
-			},
-			animation: {
-				"scale-slow": "scale-up 20000ms ease-in-out forwards",
-				"spin-slow": "spin 15s linear infinite",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-bg": "url('/public/hero-bg.jpg')",
+        "big-01": "url('/public/big01.jpg')",
+        "big-02": "url('/public/big02.jpg')",
+        serviceBanner1: "url('/public/bg-banner.png')",
+        ServiceHero: "url('/public/serviceHero.jpg')",
+      },
+      fontFamily: {
+        "Noto-Sans-JP": ["Noto Sans JP", "sans-serif"],
+      },
+      keyframes: {
+        "scale-up": {
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.25)" },
+        },
+      },
+      animation: {
+        "scale-slow": "scale-up 20000ms ease-in-out forwards",
+        "spin-slow": "spin 15s linear infinite",
+      },
+    },
+  },
+  plugins: [],
 };
