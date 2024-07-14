349f11fa5dc8ea224083335b2f90bd998f94cd04
Tholkappiar M <tholkappiar2003@gmail.com>
2024-07-14 23:06:14 +0530

    refactor: buttons in homepage

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,25 +1,26 @@
 const Hero = () => {
-	return (
-		<div className="min-h-screen flex items-center relative overflow-hidden select-none">
-			<div className="bg-hero-bg bg-center bg-no-repeat bg-cover bg-blend-overlay bg-black bg-opacity-55 h-full w-full absolute z-0 animate-scale-slow"></div>
-			<div className="container mx-auto text-white p-10 sm:p-20 xl:p-48 mt-32 flex flex-col justify-center z-10">
-				<p className="text-2xl sm:text-3xl uppercase font-Noto-Sans-JP tracking-widest font-medium">
-					Graphics. Web. Digital.
-				</p>
-				<p className="text-5xl md:text-7xl xl:text-[150px] uppercase font-Noto-Sans-JP tracking-widest font-[1000] mt-8 mb-4">
-					START-UP
-				</p>
-				<p className="w-full sm:w-[50%] font-Noto-Sans-JP tracking-wider leading-relaxed lg:tracking-widest lg:leading-loose">
-					At Pheonex Solutions, we are dedicated to providing cutting-edge
-					technology solutions that help businesses thrive in the digital age.
-					Our team of experts is ready to turn your vision into reality.
-				</p>
-				<button className="p-2 my-10 font-mono font-medium text-lg sm:text-xl bg-indigo-600 rounded-md shadow-lg w-36 sm:w-52 tracking-wider hover:opacity-90">
-					EXPLORE US
-				</button>
-			</div>
-		</div>
-	);
+    return (
+        <div className="min-h-screen flex items-center relative overflow-hidden select-none">
+            <div className="bg-hero-bg bg-center bg-no-repeat bg-cover bg-blend-overlay bg-black bg-opacity-55 h-full w-full absolute z-0 animate-scale-slow"></div>
+            <div className="container mx-auto text-white p-10 sm:p-20 xl:p-48 mt-32 flex flex-col justify-center z-10">
+                <p className="text-2xl sm:text-3xl uppercase font-Noto-Sans-JP tracking-widest font-medium">
+                    Graphics. Web. Digital.
+                </p>
+                <p className="text-5xl md:text-7xl xl:text-[150px] uppercase font-Noto-Sans-JP tracking-widest font-[1000] mt-8 mb-4">
+                    START-UP
+                </p>
+                <p className="w-full sm:w-[50%] font-Noto-Sans-JP tracking-wider leading-relaxed lg:tracking-widest lg:leading-loose">
+                    At Pheonex Solutions, we are dedicated to providing
+                    cutting-edge technology solutions that help businesses
+                    thrive in the digital age. Our team of experts is ready to
+                    turn your vision into reality.
+                </p>
+                <button className="p-2 my-10 font-mono font-medium sm:text-lg lg:text-xl bg-indigo-600 rounded-md shadow-lg w-36 sm:w-52 tracking-wider hover:opacity-90">
+                    EXPLORE US
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
--- src/Components/Home/ParallaxEffect.jsx
+++ src/Components/Home/ParallaxEffect.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const ParallaxEffect = () => {
-	return (
-		<div className="bg-big-02 h-[400px] bg-center bg-cover bg-fixed bg-no-repeat bg-black bg-opacity-55 bg-blend-overlay">
-			<div className="text-white w-full h-[100%] flex flex-col items-center justify-center">
-				<p className="text-2xl font-Noto-Sans-JP md:text-4xl font-light">
-					BEST EVER DESIGN
-				</p>
-				<p className="text-2xl md:text-5xl font-bold font-Noto-Sans-JP tracking-wider text-center m-2">
-					Time to enhance your <span className="text-indigo-600">Digital</span>{" "}
-					presence!
-				</p>
-				<Link to="/contact">
-          <button className="w-36 md:w-52 bg-indigo-600 p-2 font-Noto-Sans-JP tracking-wider rounded-md my-8 hover:bg-opacity-90">
-            CONTACT US
-          </button>
-        </Link>
-			</div>
-		</div>
-	);
+    return (
+        <div className="bg-big-02 h-[400px] bg-center bg-cover bg-fixed bg-no-repeat bg-black bg-opacity-55 bg-blend-overlay">
+            <div className="text-white w-full h-[100%] flex flex-col items-center justify-center">
+                <p className="text-2xl font-Noto-Sans-JP md:text-4xl font-light">
+                    BEST EVER DESIGN
+                </p>
+                <p className="text-2xl md:text-5xl font-bold font-Noto-Sans-JP tracking-wider text-center m-2">
+                    Time to enhance your{" "}
+                    <span className="text-indigo-600">Digital</span> presence!
+                </p>
+                <Link to="/contact">
+                    <button className="w-32 text-sm md:w-52 bg-indigo-600 p-2 font-Noto-Sans-JP tracking-wider rounded-md my-8 hover:bg-opacity-90">
+                        CONTACT US
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default ParallaxEffect;
